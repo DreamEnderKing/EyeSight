1f58b9e55401cd208897ed08e36bf2797a70302b
www20040718 <https://www20040718@dev.azure.com/www20040718/DreamSystem/_git/DreamSystem>
2020-03-16 09:27:52 +0800

    Update

---
+++ Root/Solutions/ESTool/Libs/LangSup.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： Administrator
+ * 日期: 2020/3/16
+ * 时间: 9:25
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+
+namespace Libs
+{
+	/// <summary>
+	/// Description of LangSup.
+	/// </summary>
+	public static class LangSup
+	{
+	}
+}
